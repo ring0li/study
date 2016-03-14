16de7bed093eae83556748a3a27762f758e499e3
ring0li <liuli520@gmail.com>
2016-03-14 10:07:44 +0800

    Update test.php

--- php_ext/test.php
+++ php_ext/test.php
@@ -8,7 +8,7 @@ function test($data) {
     $iv    = mcrypt_create_iv(mcrypt_get_iv_size($etype, MCRYPT_MODE_ECB), MCRYPT_RAND);
 
     //和mcrypt_encrypt($etype, $secret_key, $string, MCRYPT_MODE_CBC, $iv)一样,只是把$secret_key写死在扩展里
-    $en = walu_hello($etype, $data, MCRYPT_MODE_CBC, $iv);
+    $en = encypt_bank($etype, $data, MCRYPT_MODE_CBC, $iv);
     echo "\n加密:" . BIN2HEX($en);
 
     $de = decypt_bank($etype, $en, MCRYPT_MODE_CBC, $iv);
