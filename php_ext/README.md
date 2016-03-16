ee7453a2b090e1b37827fc1d30d92065f2dceff7
刘利 <liuli@jindanlicai.com>
2016-03-16 21:58:59 +0800

    123

--- php_ext/README.md
+++
@@ -1,4 +0,0 @@
-参考：http://walu.cc/phpbook/index.md
-https://yanbin.org/
-
-扩展：https://github.com/jonnywang/jz
--- php_ext_demo/php_jindan.h
+++ php_ext_demo/php_jindan.h
@@ -1,12 +1,13 @@
 #ifndef JINDAN_H
-
 #define JINDAN_H
-#define ENCYPT_SECRET "1234567891234567"
+
+#define ENCYPT_SECRET "1234567891234567"//长度必须16位 24位 32位
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+
 #include "php.h"
+
 #define phpext_jindan_ptr &jindan_module_entry
 extern zend_module_entry jindan_module_entry;
-
 #endif
