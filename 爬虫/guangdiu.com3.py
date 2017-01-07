aeaeab885f3ce2003fd65a4c0d181ca62a26e73e
liuli <9188917@qq.com>
2017-01-07 18:26:52 +0800

    no message

--- 爬虫/guangdiu.com3.py
+++ 爬虫/guangdiu.com3.py
@@ -1,12 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*
 
 import requests
 from pyquery import PyQuery as pq
 import os
 import time
-import datetime
+from datetime import datetime
+from datetime import timedelta
 import configparser
+import pymysql
+
+
+def get_keyword():
+    conn = pymysql.connect(host='localhost',
+                           user='root',
+                           passwd='benchi',
+                           db='liuli',
+                           charset='utf8', )
+    cursor = conn.cursor()
+    cursor.execute("select * from guangdiu_keyword where is_del=0")
+    data = cursor.fetchall()
+    keywords = []
+    for row in data:
+        keywords.append(row[1])
+    return keywords
 
 
 def get_hotest(i, e):
@@ -22,35 +39,26 @@ def get_hotest(i, e):
 
 
 def send_email(title, body):
-    print title
-    print body
-    print "\n\n"
+    print("%s %s" % (title, body))
     send_mail_command = 'echo "' + body + '" | mail -s "' + title + '" liuli@jindanlicai.com'
     os.system(send_mail_command.encode('utf-8'))
 
 
 domain = u'http://guangdiu.com'
 url_hotest = u'http://guangdiu.com/rank.php'
-
-keywords = (u'午餐肉', u'元蹄', u'扣肉罐头', u'甘竹', u'hormel', u'罐头',
-            u'生蚝', u'牛脆骨', u'冷冻生鲜',
-            u'Mane‘n Tail',
-            u'具良治',
-            u'阿瑞娜',
-            u'榴莲',
-            u'露诗',
-            u'nici',
-            u'钢琴',
-            u'竿', u'渔具', u'钓鱼', u'线组',
-            )
+keywords = []
 
 config = configparser.ConfigParser()
 config.read('guangdiu.ini')
-next_hotest_hour = config['DEFAULT']['next_hotest_hour']
-next_query_id = config['DEFAULT']['next_query_id']
+next_query_id = int(config['DEFAULT']['next_query_id'])
+next_hotest_YmdH = config['DEFAULT']['next_hotest_YmdH']
 
-print '继续执行id：', next_query_id
+print('继续执行id：', next_query_id)
 while True:
+    if (datetime.now().second == 0 or len(keywords) == 0):
+        keywords = get_keyword()
+        print(keywords)
+
     r = requests.get(domain + '/detail.php', params={'id': next_query_id})
     r.encoding = 'utf-8'
     p = pq(r.text)
@@ -65,15 +73,15 @@ while True:
         new_query_id = int(href[14:])
         # print new_id, id
         if new_query_id <= next_query_id:
-            time.sleep(10)
+            time.sleep(1)
             continue
 
     title = p(".dtitlelink").text().lower()
     for keyword in keywords:
         if title.find(keyword) != -1:
             body = u"匹配关键词：" + keyword + "\n" + url
+            print("%s %s %s %s" % (datetime.now(), next_query_id, title, url))
             send_email(title, body)
-            print "%s %s %s %s" % (datetime.datetime.now(), next_query_id, title, url)
 
     next_query_id = int(next_query_id) + 1
     config['DEFAULT']['next_query_id'] = str(next_query_id)
@@ -81,20 +89,24 @@ while True:
         config.write(configfile)
 
     current_Ymd = time.strftime("%Y%m%d")
-    current_hour = time.strftime("%H")
-    if not next_hotest_hour or int(next_hotest_hour) <= int(current_hour):
-        r = requests.get(url_hotest, params={'date': current_Ymd, 'hour': next_hotest_hour})
+    current_YmdH = time.strftime("%Y%m%d%H")
+
+    next_hotest_Ymd = time.strftime('%Y%m%d', time.strptime(next_hotest_YmdH, "%Y%m%d%H"))
+    next_hotest_H = time.strftime('%H', time.strptime(next_hotest_YmdH, "%Y%m%d%H"))
+
+    if next_hotest_YmdH <= current_YmdH:
+        data = {'date': next_hotest_Ymd, 'hour': next_hotest_H}
+        r = requests.get(url_hotest, params=data)
         r.encoding = 'utf-8'
         p = pq(r.text)
         content = p('.hourrankitem')
 
         hotest_body = ''
         content.each(get_hotest)
-        if hotest_body:
-            hotest_title = current_Ymd + str(next_hotest_hour)
-            send_email(hotest_title, hotest_body)
-
-            next_hotest_hour = int(next_hotest_hour) + 1
-            config['DEFAULT']['next_hotest_hour'] = str(next_hotest_hour)
+        if hotest_body or current_YmdH >= next_hotest_YmdH:
+            send_email(next_hotest_YmdH, hotest_body)
+            datetime_next_hour = datetime.strptime(next_hotest_YmdH, "%Y%m%d%H") + timedelta(hours=1)
+            next_hotest_YmdH = datetime_next_hour.strftime('%Y%m%d%H')
+            config['DEFAULT']['next_hotest_YmdH'] = str(next_hotest_YmdH)
             with open('guangdiu.ini', 'w') as configfile:
                 config.write(configfile)
